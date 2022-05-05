35542c1d9b4002b54564bada4ef3ce74c3c5b68e
euclidespaim <euclidespaim@gmail.com>
2022-05-05 11:42:42 -0300

    Aprimorando consultas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -241,15 +241,19 @@ class PessoaController {
         const { estudanteId } = req.params
         
         try {
-            await database.Pessoas.update(
-                { ativo: false },
-                { where: { id: Number(estudanteId) } }
-            )
-            await database.Matriculas.update(
-                { status: 'cancelado' },
-                { where: { estudante_id: Number(estudanteId) } }
-            )
-            return res.status(200).json({message: `Matrículas ref. ao estudante id: ${estudanteId} cancelada com sucesso!`})
+            database.sequelize.transaction(async transacao => {
+                await database.Pessoas.update(
+                    { ativo: false },
+                    { where: { id: Number(estudanteId) } },
+                    { transaction: transacao }
+                )
+                await database.Matriculas.update(
+                    { status: 'cancelado' },
+                    { where: { estudante_id: Number(estudanteId) } },
+                    { transaction: transacao }
+                )
+                return res.status(200).json({message: `Matrículas ref. ao estudante id: ${estudanteId} cancelada com sucesso!`})
+            })
 
         } catch (error) {
             return res.status(500).json({error: error.message})
