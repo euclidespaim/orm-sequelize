d88a1107765afb7fae82eaf0482bebea33c0982f
euclidespaim <euclidespaim@gmail.com>
2022-05-05 11:26:52 -0300

    Aprimorando consultas

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -12,13 +12,14 @@ router
     .get('/pessoas/matricula/:turmaId/confirmadas', PessoaController.pegaMatriculaPorTurma)
     .get('/pessoas/matricula/lotada', PessoaController.pegaTurmasLotadas)
     .post('/pessoas', PessoaController.criaPessoa)
-    .put('/pessoas/:id', PessoaController.atualizaPessoa)
-    .delete('/pessoas/:id', PessoaController.deletaPessoa)
     .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
-    .put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
-    .delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.deletaMatricula)
     .post('/pessoas/:id/restaura/', PessoaController.restauraPessoa)
     .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura/', PessoaController.restauraMatricula)
+    .post('/pessoas/:estudanteId/cancela/', PessoaController.cancelaPessoa)
+    .put('/pessoas/:id', PessoaController.atualizaPessoa)
+    .put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+    .delete('/pessoas/:id', PessoaController.deletaPessoa)
+    .delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.deletaMatricula)
 
 
 module.exports = router
